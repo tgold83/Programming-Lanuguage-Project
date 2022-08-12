23e0f3ae1bd85d3010017d2869412cc5d56b385d
tgold83 <ted.jonathan.gold@gmail.com>
2022-08-12 13:59:52 -0700

    add JS to make answers reload after entered once

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 // User Interface Logic
-
-function hideResults() {
+function hideResults(onsubmit) {
   document.getElementById("javaScript").setAttribute("class", "hidden");
   document.getElementById("ruby").setAttribute("class", "hidden");
   document.getElementById("c#").setAttribute("class", "hidden");
 }
 
+// Business Logic
 window.onload = function() {
   hideResults();
 
@@ -16,13 +16,13 @@ window.onload = function() {
     const favColor = document.querySelector("input[name='favColor']:checked").value;
     const morningOrNight = document.querySelector("input[name='morningOrNight']:checked").value;
     const movieGenre = document.querySelector("input[name='movieGenre']:checked").value;
-    const fav = document.querySelector("input[name='season']:checked").value;
+    const favSeason = document.querySelector("input[name='season']:checked").value;
 
       if (holdBreath >= 40 || favColor === "warmColor") {
         document.getElementById("c#").removeAttribute("class");
-      } else if (20 <= holdBreath < 40) {
+      } else if (holdBreath < 40 && movieGenre === "comedy") {
         document.getElementById("ruby").removeAttribute("class");
-      } else if (holdBreath < 20 || favSeason === "fall") {
+      } else if (favSeason === "fall") {
         document.getElementById("javaScript").removeAttribute("class");
       }
   }
